59733426891a6f9fa432d3ed7c1d309d7ec73f9a
Tarnpreet Bhatti <tarnpreetb@gmail.com>
2022-12-04 02:32:26 -0500

    -Completed days 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,10 +1,56 @@
-#![allow(unused)]
 use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    (Solution::U32(part_one()), Solution::U32(part_two()))
+}
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+fn helper_vec(input: &str) -> Vec<u32> {
+    let values: Vec<u32> = input.split(",")
+        .map(|line| line.split("-"))
+        .flatten()
+        .map(|val| val.parse::<u32>().unwrap())
+        .collect();
+
+    return values;
+}
+
+fn helper_one(input: &str) -> u32 {
+    let values: Vec<u32> = helper_vec(input);
+    let result: bool;
+
+    result = (values[0] >= values[2] && values[1] <= values[3])
+        || (values[0] <= values[2] && values[1] >= values[3]);
+
+    return result as u32;
+}
+
+fn helper_two(input: &str) -> u32{
+    let values: Vec<u32> = helper_vec(input);
+    let result: bool;
+
+    if values[0] < values[2]{
+        result = values[2] <= values[1]
+    } else {
+        result = values[0] <= values[3]
+    }
+
+    return result as u32;
+}
+
+pub fn part_one() -> u32{
+    let total_score: u32 = read_to_string("input/day04.txt").unwrap()
+        .lines()
+        .map(|line| helper_one(line))
+        .sum();
+
+    return total_score;
+}
+
+pub fn part_two() -> u32{
+    let total_score: u32 = read_to_string("input/day04.txt").unwrap()
+        .lines()
+        .map(|line| helper_two(line))
+        .sum();
+    return total_score
 }
